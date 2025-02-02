aa92648d67d9c894f04c5e77f062f0f589651084
Mih <mihaelabrodetchi@gmail.com>
2025-02-02 22:14:06 +0000

    *Project setup

---
+++ marketing-ui/src/App.jsx
@@ -0,0 +1,20 @@
+import HomeView from "./features/home/HomeView.jsx";
+
+function App() {
+  return (
+      <>
+        <HomeView/>
+      </>
+      // <Router>
+      // <div>
+      //     <Routes>
+      //         <Route path="/" element={<MarketingDashboardHome/>}/>
+      //         <Route path="/campaigns" element={<MarketingCampaignsHome/>}/>
+      //         <Route path="/channels" element={<MarketingChannelsHome/>}/>
+      //     </Routes>
+      // </div>
+      // </Router>
+  )
+}
+
+export default App
---
+++ marketing-ui/src/common/util/constants.js
@@ -0,0 +1,9 @@
+export const BASE_URL = "http://localhost:8080";
+
+export const CAMPAIGNS_URL = "/campaigns"
+
+export const CHANNELS_URL = "/channels"
+
+export const CAMPAIGN_CHANNELS_URL = "/campaign-channels"
+
+export const CAMPAIGN_CHANNEL_MAPPINGS_URL = "/campaign-channels/mappings"
