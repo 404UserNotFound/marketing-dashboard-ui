830cdcab677542f8712879ae5c4593163ab90504
Mih <mihaelabrodetchi@gmail.com>
2025-02-03 22:09:49 +0000

    *add channelListItem *add createModal *add update modal

---
+++ marketing-ui/src/features/marketing-channels/marketing_channels_modals/marketing_channel_update_modal/MarketingChannelUpdateModal.jsx
@@ -0,0 +1,54 @@
+import {Button, Modal, TextField, Typography} from "@mui/material";
+import {useState} from "react";
+import {useDispatch} from "react-redux";
+import {updateChannel} from "../../../../redux/thunks/channelThunks.js";
+
+export default function MarketingChannelUpdateModal(props) {
+    const {
+        open,
+        closeModal,
+        channels,
+        refreshChannelList
+    } = props;
+    const {
+        channelId,
+        name
+    } = channels;
+
+    const [newChannelValue, setNewChannelValue] = useState(channels);
+    const dispatch = useDispatch();
+    async function channelUpdate() {
+        await dispatch(updateChannel(newChannelValue))
+        refreshChannelList()
+    }
+
+    return (<div>
+        <Modal
+            open={open}
+            onClose={closeModal}
+        >
+            <div>
+                <Typography>Name:</Typography>
+                <TextField
+                    placeholder={name}
+                    // onChange={(e) => {setNewChannelValue({name:e.target.value})}}
+                    onChange={(e) => setNewChannelValue(prev => ({
+                        ...prev, name: e.target.value
+                    }))}
+                ></TextField>
+                <Button
+                    onClick={closeModal}
+                >Cancel
+                </Button>
+                <Button onClick={() => {
+                    channelUpdate(newChannelValue).then(
+                        closeModal
+                    )
+                }}
+                >Confirm</Button>
+            </div>
+
+        </Modal>
+    </div>)
+
+}
