21dfac37203149492f886a9b868d6420be664ef2
Mih <mihaelabrodetchi@gmail.com>
2025-02-10 19:08:32 +0000

    *delete now-redundant Home component

--- marketing-ui/src/App.jsx
+++ marketing-ui/src/App.jsx
@@ -1,9 +1,9 @@
-import HomeView from "./features/home/HomeView.jsx";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import MarketingCampaignsHome from "./features/marketing-campaigns/marketing_campaigns_home/MarketingCampaignsHome.jsx";
 import MarketingChannelsHome from "./features/marketing-channels/marketing_channels_home/MarketingChannelsHome.jsx";
 import NavTab from "./common/components/tabs/NavTab.jsx";
 import "./App.scss"
+import MarketingDashboardHome from "./features/marketing-dashboard/marketing_dashboard_home/MarketingDashboardHome.jsx";
 
 function App() {
     return (
@@ -15,7 +15,7 @@ function App() {
 
                 <div className="content-container">
                     <Routes>
-                        <Route path={"/"} element={<HomeView />} />
+                        <Route path={"/"} element={<MarketingDashboardHome />} />
                         <Route path="campaigns" element={<MarketingCampaignsHome />} />
                         <Route path="channels" element={<MarketingChannelsHome />} />
                     </Routes>
--- marketing-ui/src/features/home/HomeView.jsx
+++
@@ -1,9 +0,0 @@
-import MarketingDashboardHome from "../marketing-dashboard/marketing_dashboard_home/MarketingDashboardHome.jsx";
-
-export default function HomeView() {
-    return(
-        <>
-            <MarketingDashboardHome></MarketingDashboardHome>
-        </>
-    )
-};
