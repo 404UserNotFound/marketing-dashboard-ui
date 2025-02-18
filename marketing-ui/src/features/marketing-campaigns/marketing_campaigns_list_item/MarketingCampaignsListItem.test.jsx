ea4485d0f567d76b09d9960f5863b913846e96a0
Mih <mihaelabrodetchi@gmail.com>
2025-02-18 21:03:02 +0000

    *fixed warning *fixed redux state leaking between tests

--- marketing-ui/src/features/marketing-campaigns/marketing_campaigns_list_item/MarketingCampaignsListItem.test.jsx
+++ marketing-ui/src/features/marketing-campaigns/marketing_campaigns_list_item/MarketingCampaignsListItem.test.jsx
@@ -1,16 +1,22 @@
-import { describe, it, expect } from "vitest";
+import { describe, it } from "vitest";
 import {fireEvent, render, screen} from "@testing-library/react";import MarketingCampaignsListItem from "./MarketingCampaignsListItem.jsx";
 import {Provider} from "react-redux";
-import { mockCampaign, mockStoreWithCampaignsAndChannels} from "../../../test/testData.js";
+import {
+    createTestStore,
+    mockCampaign,
+} from "../../../test/testData.js";
 
 describe("MarketingCampaignsListItem", () => {
-    const refreshListMock = vi.fn();
     it("renders items correctly", () => {
+        const refreshListMock = vi.fn();
+        const store = createTestStore({})
+
         render(
-            <Provider store={mockStoreWithCampaignsAndChannels}>
+            <Provider store={store}>
                 <MarketingCampaignsListItem
                     campaignItem={mockCampaign}
-                    refreshList={refreshListMock} />
+                    refreshList={refreshListMock}
+                />
             </Provider>
         );
         expect(screen.getByText("Oblivion Anniversary")).toBeInTheDocument();
@@ -23,9 +29,14 @@ describe("MarketingCampaignsListItem", () => {
 
     it("opens update modal when edit button is clicked", () => {
         const refreshListMock = vi.fn();
+        const store = createTestStore({})
+
         render(
-            <Provider store={mockStoreWithCampaignsAndChannels}>
-                <MarketingCampaignsListItem campaignItem={mockCampaign} refreshList={refreshListMock} />
+            <Provider store={store}>
+                <MarketingCampaignsListItem
+                    campaignItem={mockCampaign}
+                    refreshList={refreshListMock}
+                />
             </Provider>
         );
 
@@ -36,9 +47,14 @@ describe("MarketingCampaignsListItem", () => {
 
     it("opens delete modal when edit button is clicked", () => {
         const refreshListMock = vi.fn();
+        const store = createTestStore({})
+
         render(
-            <Provider store={mockStoreWithCampaignsAndChannels}>
-                <MarketingCampaignsListItem campaignItem={mockCampaign} refreshList={refreshListMock} />
+            <Provider store={store}>
+                <MarketingCampaignsListItem
+                    campaignItem={mockCampaign}
+                    refreshList={refreshListMock}
+                />
             </Provider>
         );
 
